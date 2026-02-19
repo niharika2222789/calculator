7097558321037176373b951a9b81fc27ccd6ad94
niharika2222789 <chouhanniharika469@gmail.com>
2026-02-19 11:13:38 +0530

    Update calclator.py

--- calclator.py
+++ calclator.py
@@ -1,25 +1,31 @@
-# Simple Calculator
-
-# Get user input
 import sys
-num1 = int(sys.argv[1])
-operator = input("Enter operator (+, -, *, /): ")
-num2 = int(sys.argv[2])
 
-# Perform calculation
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    if num2 != 0:
-        result = num1 / num2
+# Check if correct number of arguments are provided
+if len(sys.argv) != 4:
+    print("Usage: python script.py <num1> <operator> <num2>")
+    sys.exit(1)
+
+try:
+    num1 = float(sys.argv[1])
+    operator = sys.argv[2]
+    num2 = float(sys.argv[3])
+
+    # Perform calculation
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: Division by zero"
     else:
-        result = "Error: Division by zero"
-else:
-    result = "Invalid operator"
+        result = "Invalid operator"
 
-# Display result
-print("Result:", result)
+    print("Result:", result)
+
+except ValueError:
+    print("Error: Please enter valid numbers.")
