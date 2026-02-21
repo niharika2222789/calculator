14c7ad71d5729e6eb9a34cc5247a31e8c7b623ce
niharika2222789 <chouhanniharika469@gmail.com>
2026-02-21 10:19:31 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,52 +1,16 @@
-import java.util.Scanner;
-
-public class calculator {
-    public static void main(String[] args) {
-        // Declare variables
-        double num1, num2, result;
-        char operator;
-        Scanner input = new Scanner(System.in);
-
-        // Prompt the user to enter numbers and operator
-        System.out.println("Enter two numbers:");
-        num1 = input.nextDouble();
-        num2 = input.nextDouble();
-
-        System.out.println("Choose an operator: +, -, *, or /");
-        operator = input.next().charAt(0);
-
-        // Perform calculation based on the operator using a switch statement
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                System.out.println(num1 + " + " + num2 + " = " + result);
-                break;
-
-            case '-':
-                result = num1 - num2;
-                System.out.println(num1 + " - " + num2 + " = " + result);
-                break;
-
-            case '*':
-                result = num1 * num2;
-                System.out.println(num1 + " * " + num2 + " = " + result);
-                break;
-
-            case '/':
-                // Handle division by zero
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    System.out.println(num1 + " / " + num2 + " = " + result);
-                } else {
-                    System.out.println("Error! Division by zero is not allowed.");
-                }
-                break;
-
-            default:
-                System.out.println("Error! Invalid operator. Please enter +, -, *, or /");
-        }
-
-        // Close the scanner
-        input.close();
-    }
+class calculator
+{
+public static void main(String args[])
+{
+int n1=Integer.parseInt(args[0]);
+int n2=Integer.parseInt(args[1]);
+int r1 = n1+n2;
+int r2 = n1-n2;
+int r3 = n1*n2;
+int r4 = n1/n2;
+System.out.println("Addition: "+r1);
+System.out.println("Subtraction: "+r2);
+System.out.println("Multiplication: "+r3);
+System.out.println("Division: "+r4);
+}
 }
